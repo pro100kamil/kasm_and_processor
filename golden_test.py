cb77e76886577276be872b6207bb40157e2f28ac
kamil <camgal04@gmail.com>
2024-05-27 20:59:35 +0300

    ci

--- golden_test.py
+++ golden_test.py
@@ -14,7 +14,7 @@ import pytest
 import translator as translator_asm
 
 
-@pytest.mark.golden_test("golden/*.yml")
+@pytest.mark.golden_test("golden/hello.yml")
 def test_translator_asm_and_machine(golden, caplog):
     """Почти полная копия test_translator_and_machine из golden_bf_test. Детали
     см. там."""
